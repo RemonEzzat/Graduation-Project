c52c5752bf6d5402d0818e1c0208b75e537dcc5a
Your Name <aabdelsayad14@gmail.com>
2020-07-16 13:58:18 +0200

    make better

--- app.js
+++ app.js
@@ -168,10 +168,5 @@ app.get("/map",function(req,res){
   app.use('/users', users);
 
 // Start Server
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-app.listen(port, function(){
-  console.log('Server started ...');
-});
+
+app.listen(process.env.PORT || 3000)
