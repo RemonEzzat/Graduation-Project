a59067e48db32aabfd011c9b90bb9c02f89d3623
Your Name <aabdelsayad14@gmail.com>
2020-07-16 13:23:54 +0200

    make better

--- app.js
+++ app.js
@@ -168,10 +168,6 @@ app.get("/map",function(req,res){
   app.use('/users', users);
 
 // Start Server
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-app.listen(port, function(){
-  console.log('Server started ...');
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
